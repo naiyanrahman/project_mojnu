a3769a722cbf2912c1dc4619dcb189bad51b576c
Naiyan AR Rahman <77504180+naiyan-official@users.noreply.github.com>
2023-07-05 05:21:14 -0700

    Update hi5_licence.sh

--- hi5_licence.sh
+++ hi5_licence.sh
@@ -1,5 +1,6 @@
-put the licence bellow
+put the license bellow
 448bce26a7b1b88dd3694d23e7ab7f57
 63a68d19ba40bf6ad3e8ba00ef6d7c6f
 448bce26a7b1b88dd3694d23e7ab7f57
 9f89c84a559f573636a47ff8daed0d33
+6ae4de40d09754e60ee3722dd462731e

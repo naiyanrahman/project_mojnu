ee674e9e666f4f76d331a37929c7ba2415819873
raselahmmed01 <137640874+raselahmmed01@users.noreply.github.com>
2023-07-06 22:35:52 +0600

    Update hi5_licence.sh

--- hi5_licence.sh
+++ hi5_licence.sh
@@ -1,7 +1,4 @@
 put the license bellow
-448bce26a7b1b88dd3694d23e7ab7f57
-63a68d19ba40bf6ad3e8ba00ef6d7c6f
-448bce26a7b1b88dd3694d23e7ab7f57
-9f89c84a559f573636a47ff8daed0d33
 6ae4de40d09754e60ee3722dd462731e test
-f257df884eaaba50c4bbabf30f230f64 naiyan 
+f257df884eaaba50c4bbabf30f230f64 naiyan
+05550cd3648a8ff2760648a0ba4edcce  mojnu
